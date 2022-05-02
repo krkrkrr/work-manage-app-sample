11d53e436e47df150b0b259eb5c2a2eeca1e24f7
krkrkrrn <67230121+krkrkrrn@users.noreply.github.com>
2022-05-02 15:15:38 +0900

    ポスト処理実装

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+function test() {
+    const test_dict = {
+        "staff": "test"
+    }
+    postSpreadsheet(test_dict)
+}
+
 /**
  * GETリクエストの処理
  * @returns {HTTP response}
@@ -10,6 +17,15 @@ function doGet() {
     return template.evaluate();
 }
 
+/**
+ * クライアント側のPOSTメソッドに対応する関数
+ * @param {Object<string, any>} [dict] column: value
+ */
+function postSpreadsheet(dict) {
+    const db = new DB()
+    db.post(dict)
+}
+
 class DB{
     /**
      * スプレッドシートのデータを取得
@@ -17,10 +33,10 @@ class DB{
     constructor() {
         this._sheet = SpreadsheetApp.getActiveSheet()
         const cells = this._sheet.getDataRange().getValues()
-        this._columns = cells.shift()
+        this._columns = ["id"].concat(cells.shift())
         this.COLUMN_DATA_TYPE = ["date", "string", "datetime", "datetime"]
-        this._rows = cells.map(row => {
-            return row.map((v, i) => {
+        this._rows = cells.map((row, i) => {
+            return [i].concat(row.map((v, i) => {
                 if(this.COLUMN_DATA_TYPE[i] == "date") {
                     return this.toStringFromDate(v)
                 } else if(this.COLUMN_DATA_TYPE[i] == "datetime") {
@@ -28,26 +44,26 @@ class DB{
                 } else {
                     return v
                 }
-            })
+            }))
         })
     }
 
     /**
      * Date型を文字列へ変換
-     * @param {Date} date 
-     * @param {Object} option 
+     * @param {Date} [date] 
+     * @param {Object} [option] 
      * @returns {string} YYYY/MM/DD hh:mm::ss or hh:mm or ""
      */
-    toStringFromDate(date, option) {
+    toStringFromDate(date = new Date(), option = {}) {
         const isInvalidDate = (arg) => Number.isNaN((new Date(arg)).getTime())
         if(!isInvalidDate(date)) {
             if(option && "datetime" in option) {
-                return date.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})
+                return (new Date(date)).toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})
             } else {
-                return date.toLocaleDateString("ja-JP")
+                return (new Date(date)).toLocaleTimeString("ja-JP")
             }
         } else {
-            return ""
+            return date
         }
     }
 
@@ -64,5 +80,36 @@ class DB{
      */
     get rows() {
         return this._rows
+    }
+
+    /**
+     * 条件に一致する行を取得する
+     * 条件を指定しない場合は全取得
+     * @param {Array<string>} [columns] 複数のカラム
+     * @param {Array<>} [values] 複数の完全一致する条件
+     * @returns {Array} 条件に一致する行
+     */
+    get(columns = [], values = []) {
+        let res = this.rows
+        for(const i in columns) {
+            const col_index = this._columns.indexOf(columns[i])
+            if(col_index < 0) {
+                continue
+            }
+            res = res.filter(row => row[col_index] == values[i])
+        }
+        return res        
+    }
+
+    /**
+     * スプレッドシートにpost
+     * @param {Object<string, any>} [dict] postするcolumn: value 
+     */
+    post(dict = {}) {
+        dict["timestamp"] = new Date()
+        const row = this.columns
+            .map(column => column in dict ? dict[column] : "")
+        row.shift()
+        this._sheet.appendRow(row)
     }
 }
